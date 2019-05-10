7e4598cb5b651ad80190f761072d89b6b9bdda95
l1707823672 <1707823672@qq.com>
2019-05-10 23:18:35 +0800

    update

--- shopping/pages/address/address.js
+++ shopping/pages/address/address.js
@@ -1,68 +1,78 @@
-// pages/category/category.js
+// pages/address/address.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    category: [
-      { name: '果味', id: 'guowei' },
-      { name: '蔬菜', id: 'shucai' },
-      { name: '炒货', id: 'chaohuo' },
-      { name: '点心', id: 'dianxin' },
-      { name: '粗茶', id: 'cucha' },
-      { name: '淡饭', id: 'danfan' }
-    ],
-    curIndex: 0,
-    toView: 'guowei',
-    isScroll: false,
-    heightArr: []
+    address:{
+    name:'',
+    photo:'',
+    detail:''
+    }
   },
-    switchTab(e){
-      console.log(e);
-      this.setData({
-        toView:e.target.dataset.id,
-        curIndex:e.target.dataset.index,
+  bindName(e){
+    this.setData({
+      'address.name':e.detail.value
+    })
+  },
+  bindPhone(e){
+    this.setData({
+      'address.phone':e.detail.value
+    })
+  },
+  bindDetail(e){
+    this.setData({
+      'address.detail':e.detail.value
+    })
+  },
+  formSubmit(){
+    if(this.data.address.name && this.data.address.phone && this.data.address.detail){
+      wx.setStorage({
+        key: 'address',
+        data: this.data.address,
+        success: (result) => {
+          wx.navigateBack()
+        },
+        fail: () => {},
+        complete: () => {}
+      });
+        
+    }else{
+      wx.showModal({
+        title:'提示',
+        content:'请填写完整资料',
+        showCancel:false
       })
-    },
-    
-   
+    }
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
-    let self = this
-    wx.request({
-      url: 'http://www.gdfengshuo.com/api/wx/cate-detail.txt',
-      success(res) {
-        self.setData({
-          detail: res.data,
-         
-        })
-      }
-    })
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
--- shopping/pages/cart/cart.js
+++ shopping/pages/cart/cart.js
@@ -7,7 +7,8 @@ Page({
   data: {
       hasList:false,
       carts:[],
-      selectAllStatus:true
+      selectAllStatus:true,
+      totalPrice:null
   },
 
   /**
@@ -32,11 +33,11 @@ Page({
       this.setData({
         hasList:true,
         carts: [
-          { id: 1, title: '新鲜芹菜 半斤', image: '/image/s5.png', num: 4, price: 0.01, selected: false },
+          { id: 1, title: '新鲜芹菜 半斤', image: '/image/s5.png', num: 4, price: 0.01, selected: true },
           { id: 2, title: '素米 500g', image: '/image/s6.png', num: 1, price: 0.03, selected: true }
         ]
-
       })
+      this.getTotalPrice()
     },1000)
   },
   selectAll(){
@@ -50,6 +51,61 @@ Page({
       selectAllStatus:selectAllStatus,
       carts:carts
     })
+    this.getTotalPrice()
+  },
+  getTotalPrice(){
+    let carts = this.data.carts
+    let total =0
+    for(let i=0;i < carts.length; i++ ){
+      if(carts[i].selected){
+        total += carts[i].num * carts[i].price
+      }
+    }
+    this.setData({
+      totalPrice:total.toFixed(2)
+    })
+  },
+  minusCount(e){
+    // console.log(e);
+    const index =e.target.dataset.index
+    let carts = this.data.carts 
+    let num = carts[index].num
+    if(num<=1){
+      return 
+    }
+    num = num -1
+    carts[index].num=  num 
+    this.setData({
+      carts:carts
+    })
+    this.getTotalPrice()
+  },
+  addCount(e){
+    const index =e.target.dataset.index
+    let carts = this.data.carts 
+    let num = carts[index].num
+    num = num +1
+    carts[index].num=  num 
+    this.setData({
+      carts:carts
+    })
+    this.getTotalPrice()
+  },
+  deleteList(e){
+    const index =e.target.dataset.index
+    let carts=this.data.carts
+    carts.splice(index,1)
+    this.setData({
+      carts:carts
+    })
+    if(!carts.length){
+      this.setData({
+        hasList:false,
+      })
+    }else{
+      this.getTotalPrice()
+    }
+
   },
   /**
    * 生命周期函数--监听页面隐藏
